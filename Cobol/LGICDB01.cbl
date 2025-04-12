ce6f4e0f5d1efa218669c9ea74443b543baeaf6b
wfezzani <wissem.fezzani@fr.ibm.com>
2025-04-12 05:05:45 +0200

    modif25

--- Cobol/LGICDB01.cbl
+++ Cobol/LGICDB01.cbl
@@ -18,7 +18,7 @@
       * Select customer details from DB2 table                         *
       *                                                                *
       *                                                                *
-      *modif24                                                *
+      *modif25                                                *
 
       ******************************************************************
        IDENTIFICATION DIVISION.
